202b658c3879d4912fa96a7fb0acbe8cff62187d
ahmed-khaldi-fin <akhaldi@finaxys.com>
2018-05-16 14:08:45 +0200

    - with datasets

--- src/main/java/com/sparxys/SparkExample.java
+++ src/main/java/com/sparxys/SparkExample.java
@@ -2,12 +2,15 @@ package com.sparxys;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.MapFunction;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
@@ -21,15 +24,15 @@ public class SparkExample {
         Date date = new Date();
         String today = dateFormat.format(date);
 
-        JavaRDD<String> data = sc.textFile("input.csv");
-        data.map(row -> row+=","+today).collect().forEach(System.out::println);
+        //JavaRDD<String> data = sc.textFile("/home/finaxys/Projects/sparkJobs/input.csv");
+        //data.map(row -> row+=","+today).take(8).forEach(System.out::println);
         
         SparkSession spark = SparkSession.builder().getOrCreate();
         spark.sql("set spark.sql.orc.impl=native");
         
-        Dataset<Row> df = spark.read().csv("input.csv");
-        //df.map(row -> row+=","+today , Encoders.STRING());
-        df.write().format("orc").save("spark-job");
+        Dataset<Row> df = spark.read().csv("/home/finaxys/Projects/sparkJobs/input.csv");
+
+        df.map((MapFunction<Row, String>) row -> row +","+today, Encoders.STRING()).write().format("orc").save("spark-job");
 
         sc.close();
     }
